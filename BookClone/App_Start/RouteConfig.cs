dce32a31b12435f53cf0373b83dcff03e84f8476
saimanisha <saimanisha@>
2018-12-10 07:03:13 +0530

    bookclone sample

--- BookClone/App_Start/RouteConfig.cs
+++ BookClone/App_Start/RouteConfig.cs
@@ -12,6 +12,13 @@ namespace BookClone
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapMvcAttributeRoutes();
+            routes.MapRoute(
+                "MoviesByReleaseDate",
+                "movies/released/{year}/{month}",
+                new { controller = "Movies", action = "ByReleaseDate" },
+                new { year = @"/d{4}", month = @"/d{2}" }
+                );
 
             routes.MapRoute(
                 name: "Default",
---
+++ BookClone/Controllers/MoviesController.cs
@@ -0,0 +1,52 @@
+﻿using BookClone.Models;
+using BookClone.viewmodels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookClone.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie()
+            {
+                Name = "Kedarnath"
+            };
+            var customers = new List<Customer> {
+                new Customer { Name ="saimanisha 1"},
+                new Customer{  Name="saimanisha 2"},
+            };
+            //ViewData["Movie"] = movie;
+            //ViewBag.Movierandom = movie;
+            var viewmodel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+
+            };
+            return View(viewmodel);
+        }
+        //public ActionResult Index(int? PageIndex,String Sortby)
+        //{
+        //    if (!PageIndex.HasValue)
+        //        PageIndex = 1;
+
+        //    if (string.IsNullOrWhiteSpace(Sortby))
+        //        Sortby = "Name";
+
+        //    return Content(string.Format("PageIndex={0}&Sortby={1}", PageIndex, Sortby));
+
+        //}
+        //public ActionResult ByReleaseDate(int year, int month)
+        //{
+
+        //    return Content(year + "this is year" + "  " + month + "this is month");
+        //}
+    }
+    
+}
