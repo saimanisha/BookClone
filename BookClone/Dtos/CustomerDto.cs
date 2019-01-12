73d135d0b4cd4e15719b2255f3c2a160a283b6c7
saimanisha <saimanisha@>
2019-01-12 07:45:41 +0530

    12-01-19

---
+++ BookClone/Dtos/CustomerDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using BookClone.Models;
+
+namespace BookClone.Dtos
+{
+    public class CustomerDto
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(259)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+
+     //   [Min18YearsIfAMember]
+        public DateTime? Birthdate { get; set; }
+
+    }
+}
